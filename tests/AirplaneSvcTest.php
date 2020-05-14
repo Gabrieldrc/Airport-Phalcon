1cf253ca170cdc108e3884010bd10d53bd66233d
Gabrieldrc <gabriel.drc25@gmail.com>
2020-05-14 16:04:34 -0300

    test directory

---
+++ tests/AirplaneSvcTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class AirplaneSvcTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        $this->service = new AirplaneService();
+        $this->reflection = new \ReflectionClass(get_class($this->service));
+    }
+    
+    public function invokeMethod(&$object, $methodName, array $parameters = array())
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
+    }
+
+
+    public function testFunctionName(): void
+    {
+        $return = $this->invokeMethod(
+            $this->reflection,
+            'newAirplane',
+            ['location',25]);
+        var_dump($return);
+        $this->assertTrue(True);
+    }
+}
